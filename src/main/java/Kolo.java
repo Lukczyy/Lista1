a7733901ab558c1e3fb79c628b87e4423f9f27c1
Łukasz Czyrko <lukczyke@gmail.com>
2021-10-21 07:29:07 +0200

    Adding few more classes

---
+++ src/main/java/Kolo.java
@@ -0,0 +1,12 @@
+public class Kolo extends Figura{
+    double r;
+    @Override
+    public double ObliczPole() {
+        return Math.PI*r*r;
+    }
+
+    @Override
+    public double ObliczObwod() {
+        return 2*Math.PI*r;
+    }
+}
---
+++ src/main/java/Obliczenia.java
@@ -0,0 +1,4 @@
+public interface Obliczenia {
+    double ObliczPole();
+    double ObliczObwod();
+}
